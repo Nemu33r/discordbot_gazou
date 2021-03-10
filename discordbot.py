818dcf82c95022bb7862970c53a11c5e448df685
Nemu33r <nemu.33r@gmail.com>
2021-03-10 20:20:02 +0900

    日本語でのコメントテスト。機能追加、redisの導入準備。

--- discordbot.py
+++ discordbot.py
@@ -13,6 +13,11 @@ async def on_ready():
     # 起動したらターミナルにログイン通知が表示される
     print('ログインしました')
 
+# 返信する非同期関数を定義
+async def reply(message):
+    reply = f'{message.author.mention} Usageを出したいなあ' # 返信メッセージの作成
+    await message.channel.send(reply) # 返信メッセージを送信
+
 # メッセージ受信時に動作する処理
 @client.event
 async def on_message(message):
@@ -26,6 +31,17 @@ async def on_message(message):
     # 「/hoge」と発言したら「なんか」が返る処理
     if message.content == '/gaming_shoe':
         await message.channel.send('https://cdn.discordapp.com/attachments/819156022287532053/819156068046340136/image0.gif')
+
+    # 「/hoge」と発言したら「なんか」が返る処理
+    if message.content == '/abiko':
+        await message.channel.send('自サーバのそと https://cdn.discordapp.com/attachments/726092371799965789/818824755704102982/image0.gif')
+
+    # 「/hoge」と発言したら「なんか」が返る処理
+    if message.content == '/lonkis':
+        await message.channel.send('DriveのGifはさんしょうできないです https://cdn.discordapp.com/attachments/726092371799965789/818824755704102982/image0.gif')
+
+    if client.user in message.mentions: # 話しかけられたかの判定
+        await reply(message) # 返信する非同期関数を実行
 
 # Botの起動とDiscordサーバーへの接続
 client.run(TOKEN)
