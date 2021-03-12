700c361229db59125535714b0961a01729de219e
Nemu33r <nemu.33r@gmail.com>
2021-03-13 00:24:50 +0900

    本番環境用にrを修正

--- r.py
+++ r.py
@@ -8,16 +8,16 @@ import env
 
 def connect():
     #環境変数に既にREDIS_URLがあるなら
-    #print(os.environ.get('REDIS_URL'))
-    #return redis.from_url(
-    #    url=os.environ.get('REDIS_URL'), # 環境変数にあるURLを渡す
-    #    decode_responses=True, # 日本語の文字化け対策のため必須
-    #)
+    print(os.environ.get('REDIS_URL'))
+    return redis.from_url(
+        url=os.environ.get('REDIS_URL'), # 環境変数にあるURLを渡す
+        decode_responses=True, # 日本語の文字化け対策のため必須
+    )
 
     #ローカル環境等で環境変数にREDIS_URLがないなら
-    print('Redisに接続します')
-    return redis.from_url(
-        url=env.REDIS_URL,
-        decode_responses=True,
-
-    )
+    #print('Redisに接続します')
+    #return redis.from_url(
+    #    url=env.REDIS_URL,
+    #    decode_responses=True,
+    #
+    #)
